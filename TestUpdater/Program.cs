7ddd50ab5acf3bf130350cecf8524ec9fc2f2079
xiyangz <1213278475@qq.com>
2020-12-15 19:11:00 +0800

    add many details

---
+++ TestUpdater/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace TestUpdater
+{
+    class Program
+    {
+        public static string update_url = "https://update.aerosim.com.cn/aeroconnector_update/";
+        public static int current_version = 100;
+
+        static void Main(string[] args)
+        {
+            //try
+            //{
+                string update_info = GetUpdateInfoFromUrl(update_url + "versions.json");
+                JObject jobj = JObject.Parse(update_info);
+                int version = (int)jobj["current_ver"];
+                if (current_version < version)
+                {//有新版本
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = "../Updater.exe"; //启动的应用程序名称
+                    startInfo.Arguments = "-v " + current_version + " -l zh-CN -u " + update_url + " -j " + update_info;
+                    startInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    Process.Start(startInfo);
+                }
+            //}
+           // catch (Exception)
+            //{
+            //    Console.WriteLine("Error Occur");
+            //}
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static public string GetUpdateInfoFromUrl(string http_url)
+        {
+            WebRequest request = WebRequest.Create(http_url);
+            string urlContent = "";
+            byte[] read = new byte[1024];
+            Stream netStream = request.GetResponse().GetResponseStream();
+            int realReadLen = netStream.Read(read, 0, read.Length);
+            while (realReadLen > 0)
+            {
+                urlContent += System.Text.Encoding.Default.GetString(read);
+                realReadLen = netStream.Read(read, 0, read.Length);
+            }
+            return urlContent;
+        }
+    }
+}
--- Update/MainWindow.xaml.cs
+++ Update/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
 
 namespace Update
 {
@@ -26,114 +17,150 @@ namespace Update
         public MainWindow()
         {
             InitializeComponent();
-
-            ThreadPool.QueueUserWorkItem((obj) =>
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {//进行json请求
+            try
             {
-
-                String[] http_urls = new string[] { "https://repo.anaconda.com/archive/Anaconda3-2020.11-Windows-x86_64.exe",
-                                                "https://download-cf.jetbrains.com/python/pycharm-community-2020.3.exe",
-                                                   };
-                String[] save_urls = new string[] { @"1.zip",
-                                                @"2.zip",
-                                                   };
-                List<String> exist_http_urls = new List<string>();
-                List<String> exist_save_urls = new List<string>();
-                for (int i = 0; i < http_urls.Length; i++)
+                string[] pargs = Environment.GetCommandLineArgs();
+                //保存 -v当前版本 -u更新地址 -l显示语言 -j版本json文件   更新地址+版本文件能得到文件地址
+                Dictionary<string, string> cmdParam = new Dictionary<string, string>();
+                string exePath = System.IO.Path.GetDirectoryName(pargs[0]);
+                tb_test.FontSize = 10;
+                for (int i = 1; i < pargs.Length; i += 2)
                 {
-                    if (HttpFileExist(http_urls[i]))
-                    {
-                        exist_http_urls.Add(http_urls[i]);
-                        exist_save_urls.Add(save_urls[i]);
-                    }
+                    tb_test.Text += pargs[i] + pargs[i + 1];
+                    cmdParam.Add(pargs[i], pargs[i + 1]);
                 }
-                DownloadHttpFile(exist_http_urls, exist_save_urls);
+                int current_version = int.Parse(cmdParam["-v"]);//版本，如：100
+                string sel_language = cmdParam["-l"]; //语言，如：zh-CN
+                string update_url = cmdParam["-u"];  //地址，如: https://update.aerosim.com.cn/aeroconnector_update/
+                //找到需要迭代的版本  比如从 100 升级到 103 需要迭代101 102 103三个版本
+                JObject jobj = JObject.Parse(GetUpdateInfoFromUrl(update_url + "versions.json"));//json字符串{"current_ver": 102,"vesions": [{ "102": 101},{"101" : 100},{ "100" : 0}]}
+                int newest_version = (int)jobj["current_ver"];
+                List<int> span_versions = new List<int>();
+                int ver = (int)jobj["vesions"][Convert.ToString(newest_version)];
+                while(ver != current_version)
+                {
+                    span_versions.Add(ver);
+                    ver = (int)jobj["vesions"][Convert.ToString(ver)];
+                }
+                //1.剔除旧版本在新版本也更新的文件，2.增加旧版本新增新版本没更新的文件 和 3.增加新版本中旧版本没有的文件
+
+
+
+                string version_str = (string)jobj["version_str"];
+                int version = (int)jobj["version"];
+                string update_info = (string)jobj["update_info"];
+                List<string> file_names = new List<string>();
+                List<long> file_sizes = new List<long>();
+                List<string> dwld_urls = new List<string>();
+                List<string> save_urls = new List<string>();
+
+                foreach (var file in jobj["files"])
+                {
+                    file_names.Add((string)file["file_name"]);
+                    file_sizes.Add((long)file["file_size"]);
+                    dwld_urls.Add((string)file["download_url"]);
+                    save_urls.Add((string)file["save_url"] + (string)file["file_name"]);
+                }
+
+                tb_info.Text = update_info;
+                tb_version.Text += version_str;
+
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    //List<String> exist_http_urls = new List<string>();
+                    //List<String> exist_save_urls = new List<string>();
+                    //for (int i = 0; i < http_urls.Length; i++)
+                    //{
+                    //    if (HttpFileExist(http_urls[i]))
+                    //    {
+                    //        exist_http_urls.Add(http_urls[i]);
+                    //        exist_save_urls.Add(save_urls[i]);
+                    //    }
+                    //}
+                    DownloadHttpFile(file_names, file_sizes, dwld_urls, save_urls);
+                }
+                , null);
             }
-            , null);
-            
+            catch (Exception)
+            {
+                MessageBox.Show("出现错误，更新失败");
+            }
+        }
+        public string GetUpdateInfoFromUrl(string http_url)
+        {
+            WebRequest request = WebRequest.Create(http_url);
+            string urlContent = "";
+            byte[] read = new byte[1024];
+            Stream netStream = request.GetResponse().GetResponseStream();
+            int realReadLen = netStream.Read(read, 0, read.Length);
+            while (realReadLen > 0)
+            {
+                urlContent += System.Text.Encoding.Default.GetString(read);
+                realReadLen = netStream.Read(read, 0, read.Length);
+            }
+            return urlContent;
         }
 
         //需要处理下载失败
-        public void DownloadHttpFile(List<String> http_urls, List<String> save_urls)
+        public void DownloadHttpFile(List<string> file_names, List<long> file_sizes, List<string> http_urls, List<string> save_urls)
         {
-            WebResponse response = null;
-            //获取远程文件
-            List<WebRequest> requests = new List<WebRequest>();
-            List<String> fileNames = new List<String>();
-            int i = 0;
             long max = 0;
-            foreach (var urls in http_urls)
+            foreach (var size in file_sizes)
             {
-                requests.Add(WebRequest.Create(urls));
-                response = requests[i].GetResponse();
-                if (response == null) return;
-                //读远程文件的大小
-                max += response.ContentLength;
-                
-                fileNames.Add(response.ResponseUri.Segments[response.ResponseUri.Segments.Length - 1]);
-                i++;
+                max += size;
             }
             pbDown.Dispatcher.BeginInvoke(new ProgressBarMaximumSetter(SetProgressBarMaximum), max);
 
             //下载远程文件
-            ThreadPool.QueueUserWorkItem((obj) =>
+            //获取远程文件
+            List<WebRequest> requests = new List<WebRequest>();
+            byte[] read = new byte[1024 * 1024];
+            long readCount = 0;
+            int downloadSpeed = 0;
+            int interval_min = 50;
+            int interval_max = 800;
+            long interval = interval_min;
+            long tick = 0;
+            for (int k=0; k< http_urls.Count; k++)
             {
-                long readCount = 0;
-                for (int k=0; k< requests.Count; k++)
+                requests.Add(WebRequest.Create(http_urls[k]));
+                while (IsFileInUse(save_urls[k]))
                 {
-                    Stream netStream = requests[k].GetResponse().GetResponseStream();
-                    Stream fileStream = new FileStream(save_urls[k], FileMode.Create);
-                    byte[] read = new byte[1024];
-                    int realReadLen = netStream.Read(read, 0, read.Length);
-                    long oldTick = DateTime.Now.Ticks / 10000;
-                    long interval = 0;
-                    int downloadSpeed = 0;
-                    while (realReadLen > 0)
+                    sp2.Dispatcher.BeginInvoke(new FileUsedSetter(DisplayFileUsed), file_names[k]);
+                    Thread.Sleep(500);
+                }
+                Stream fileStream = new FileStream(save_urls[k], FileMode.Create);
+                Stream netStream = requests[k].GetResponse().GetResponseStream();
+                long oldTick = DateTime.Now.Ticks / 10000;
+                int realReadLen = netStream.Read(read, 0, read.Length);
+                while (realReadLen > 0)
+                {
+                    readCount += (long)realReadLen;
+                    tick = (DateTime.Now.Ticks / 10000) - oldTick;
+                    if (tick > interval)
                     {
-                        readCount += (long)realReadLen;
-                        interval = (DateTime.Now.Ticks / 10000) - oldTick;
-                        if (interval > 800)
-                        {
-                            downloadSpeed = (int)(readCount * 1000L / 1024L / interval);
-                            readCount = 0;
-                            oldTick = DateTime.Now.Ticks / 10000;
-                        }
-                        fileStream.Write(read, 0, realReadLen);
-                        //progressBarValue += realReadLen;
-                        object[] param = new object[] { realReadLen, fileNames[k], downloadSpeed};
-                        pbDown.Dispatcher.BeginInvoke(new ProgressBarSetter(SetProgressBar), param);
-                        realReadLen = netStream.Read(read, 0, read.Length);
-
-                       
+                        downloadSpeed = (int)(readCount * 1000L / 1024L / tick);
+                        readCount = 0;
+                        oldTick = DateTime.Now.Ticks / 10000;
+                        if (interval < interval_max)
+                            interval += 50;
                     }
-                    fileStream.Close();
-                    netStream.Close();
-                }
-                
-            }, null);
-           
-        }
 
-        private bool HttpFileExist(string http_file_url)
-        {
-            WebResponse response = null;
-            bool result = false;//下载结果
-            try
-            {
-                response = WebRequest.Create(http_file_url).GetResponse();
-                result = response == null ? false : true;
-            }
-            catch (Exception ex)
-            {
-                result = false;
-            }
-            finally
-            {
-                if (response != null)
-                {
-                    response.Close();
+                    fileStream.Write(read, 0, realReadLen);
+                    //progressBarValue += realReadLen;
+                    object[] param = new object[] { realReadLen, file_names[k], downloadSpeed };
+                    sp2.Dispatcher.BeginInvoke(new ProgressBarSetter(SetProgressBar), param);
+                    realReadLen = netStream.Read(read, 0, read.Length);
+                    Thread.Sleep(1);
+                       
                 }
+                fileStream.Close();
+                netStream.Close();
             }
-            return result;
+           
         }
 
         public delegate void ProgressBarMaximumSetter(long max);
@@ -175,6 +202,42 @@ namespace Update
             }
             tb_percent.Text += remain_sec + "s ";
             tb.Text = fileName;
+        }
+
+        public delegate void FileUsedSetter(string fileName);
+        public void DisplayFileUsed(string fileName)
+        {
+            tb.Text = "\""+ fileName + "\"" + "正在被其他程序占用...";
+            tb_percent.Text = ((pbDown.Value / pbDown.Maximum) * 100).ToString("f1") + "%";
+        }
+     
+
+        //监测文件是否被其他软件使用
+        public static bool IsFileInUse(string fileName)
+        {
+            bool inUse = true;
+
+            FileStream fs = null;
+            try
+            {
+
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read,
+
+                FileShare.None);
+
+                inUse = false;
+            }
+            catch(Exception)
+            {
+
+            }
+            finally
+            {
+                if (fs != null)
+
+                    fs.Close();
+            }
+            return inUse;//true表示正在使用,false没有使用
         }
     }
 }
